eae1929f17001767bccf93f2e8dab83f05d096ca
student <wanjirushee@gmail.com>
2020-08-02 18:27:29 +0300

    Fruits and vegetables display

---
+++ src/MyComponents.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+class Spinach extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type :"Vegetables",
+      name: "Spinach",
+      quantity:200,
+      
+    };
+  }
+  render() {
+    return (
+      <div class = "one">
+        <img src = "/images/spinach.jpeg" alt="" />
+        <ul>
+          <p>Type:{this.state.type}</p>
+          <p>Name:{this.state.name}</p>
+          <p>Quantity:{this.state.quantity}</p>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Onions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "Vegetables",
+      name: "Onions",
+      quantity:300,
+    
+    };
+  }
+  render() {
+    return (
+      <div class = "two">
+        <img src = "/images/onions.jpeg" alt=""  />
+        <ul>
+          <p>Type:{this.state.type}</p>
+          <p>Name:{this.state.name}</p>
+          <p>Quantity:{this.state.quantity}</p>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+class Oranges extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: "Fruits",
+      name: "Oranges",
+      quantity:250,
+    
+    };
+  } 
+  render() {
+    return (
+      <div class = "three">
+    <MyNavBar />
+        <img src = "/images/oranges.jpeg" alt = "" />
+        <ul>
+          <p>Type:{this.state.type}</p>
+          <p>Name:{this.state.name}</p>
+          <p>Quantity:{this.state.quantity}</p>
+        </ul>
+        <Spinach />
+        <Onions />
+      </div>
+    );
+  }
+}
+ 
+
+ function MyNavBar(){
+  return (
+    <div class= "four">
+      <body>
+      <header>
+      <nav>
+          <ul>
+              <li><a href = "Home.html">Home</a></li>
+              <li><a href = "About.html">About</a></li>
+              <li><a href = "products.html">Products</a></li>
+              <li><a href = "cart.html">Cart</a></li>
+              <li><a href = "contacts.html">Contacts</a></li>
+          </ul>
+        </nav>
+        </header>
+        </body>
+    </div>
+  );    
+}
+
+export default Oranges;
